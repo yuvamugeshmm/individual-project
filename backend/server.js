7333caf5878a77646b1eef318a827f44a0466386
Yuva Mugesh <yuvamugeshmm23it@srishakthi.ac.in>
2026-01-28 22:11:44 +0530

    Added railway config files

--- backend/server.js
+++ backend/server.js
@@ -21,11 +21,13 @@ app.use(helmet({
 app.use(cors({
   origin: [
     process.env.FRONTEND_URL,
+    'https://individual-project.vercel.app',
     'http://localhost:3000',
     'http://127.0.0.1:3000',
     'http://localhost:5173',
     'http://127.0.0.1:5173'
   ].filter(Boolean),
+
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
