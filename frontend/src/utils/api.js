deb04569a814e51df285891000cb10a6387bd0ae
yuvamugeshmm <yuvamugesh9@gmail.com>
2026-01-31 11:30:10 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,14 +1,18 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5000/api"
+    : "https://individual-project-1-a5b2.onrender.com/api"; 
 
 const api = axios.create({
   baseURL: API_URL,
   withCredentials: true,
   headers: {
-    'Content-Type': 'application/json'
-  }
+    "Content-Type": "application/json",
+  },
 });
+
 
 // Auth APIs
 export const login = (studentId, password) => {
